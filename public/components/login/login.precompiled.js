4e229b46174f945f72bdba83d92df8cfd403f2ef
almkuznetsov <almkuznetsov@mail.ru>
2023-10-01 17:12:42 +0300

    background logo, login and signup layout added

--- public/components/login/login.precompiled.js
+++ public/components/login/login.precompiled.js
@@ -1,6 +1,6 @@
 (function() {
   var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
 templates['login.hbs'] = template({"compiler":[8,">= 4.3.0"],"main":function(container,depth0,helpers,partials,data) {
-    return "<form class=\"login\">\n    <span class=\"text-center\">вход</span>\n    <div class=\"input-container\">\n        <input id=\"mail\" type=\"text\" required=\"\" />\n        <label>Логин</label>\n    </div>\n    <div class=\"input-container\">\n        <input id=\"password\" type=\"text\" required=\"\" />\n        <label>Пароль</label>\n    </div>\n    <div class=\"error-label\"></div>\n    <div class=\"auth-subtitle\">\n        Нет аккаунта?\n        <a href=\"signup.html\">Зарегестрируйтесь</a>\n    </div>\n    <button type=\"submit\" class=\"sign-btn\">войти</button>\n</form>";
+    return "<img src=\"../../static/img/logo_ad.png\" alt=\"Логотип\" class=\"logo\">\n<form class=\"login\">\n        <h1>Начни свою рекламную кампанию</h1>\n        <p>Войти в аккаунт.</p>\n        <p>Еще нет аккаунта? <a href=\"#\">Зарегистрироваться</a></p>\n\n        <input type=\"text\" placeholder=\"Логин\">\n        <input type=\"password\" placeholder=\"Пароль\">\n        <button>Войти</button>\n</form>";
 },"useData":true});
 })();
