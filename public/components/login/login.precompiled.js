82257eac2bf9cc2e7e57dbacff8a67ec2c09a927
BincomAD <DeezPlay2003@gmail.com>
2023-10-04 00:11:55 +0300

    Add IDs

--- public/components/login/login.precompiled.js
+++ public/components/login/login.precompiled.js
@@ -1,6 +1,6 @@
 (function() {
   var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
 templates['login.hbs'] = template({"compiler":[8,">= 4.3.0"],"main":function(container,depth0,helpers,partials,data) {
-    return "\n<form class=\"login\">\n        <h1>Начни свою рекламную кампанию</h1>\n        <p>Войти в аккаунт.</p>\n        <p>Еще нет аккаунта? <a href=\"/signup\">Зарегистрироваться</a></p>\n\n        <input type=\"text\" placeholder=\"Логин\" id=\"login\">\n        <input type=\"password\" placeholder=\"Пароль\" id=\"password\">\n        <div class=\"error-label\"></div>\n        <button>Войти</button>\n</form>";
+    return "\n<form class=\"login\">\n        <h1>Начни свою рекламную кампанию</h1>\n        <p>Войти в аккаунт.</p>\n        <p>Еще нет аккаунта? <a href=\"/signup\">Зарегистрироваться</a></p>\n\n        <input type=\"email\" placeholder=\"Логин\" id=\"login\">\n        <input type=\"password\" placeholder=\"Пароль\" id=\"password\">\n        <div class=\"error-label\"></div>\n        <button>Войти</button>\n</form>";
 },"useData":true});
 })();
--- public/components/signup/signup.precompiled.js
+++ public/components/signup/signup.precompiled.js
@@ -1,6 +1,6 @@
 (function() {
   var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
 templates['signup.hbs'] = template({"compiler":[8,">= 4.3.0"],"main":function(container,depth0,helpers,partials,data) {
-    return "<form class=\"signup\">\n    <h1>Начни свою рекламную кампанию</h1>\n    <h2>Создать новый аккаунт</h2>\n    <p>Уже есть аккаунт? <a href=\"/login\">Войти</a></p>\n\n    <input type=\"text\" placeholder=\"Имя\">\n    <input type=\"text\" placeholder=\"Фамилия\">\n    <input type=\"email\" placeholder=\"Почта\">\n    <input type=\"password\" placeholder=\"Пароль\">\n    <div class=\"error-label\"></div>\n    <button>Создать аккаунт</button>\n</form>";
+    return "<form class=\"signup\">\n    <h1>Начни свою рекламную кампанию</h1>\n    <h2>Создать новый аккаунт</h2>\n    <p>Уже есть аккаунт? <a href=\"/login\">Войти</a></p>\n\n    <input type=\"text\" placeholder=\"Имя\" id=\"f_name\">\n    <input type=\"text\" placeholder=\"Фамилия\" id=\"l_name\">\n    <input type=\"email\" placeholder=\"Почта\" id=\"login\">\n    <input type=\"password\" placeholder=\"Пароль\" id=\"password\">\n    <div class=\"error-label\"></div>\n    <button>Создать аккаунт</button>\n</form>";
 },"useData":true});
 })();
