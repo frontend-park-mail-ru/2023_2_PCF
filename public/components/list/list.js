a721665c140c1f92cd36074a9ae61561d51bec74
almkuznetsov <almkuznetsov@mail.ru>
2023-10-04 01:19:33 +0300

    eslint errors fixed

--- public/components/list/list.js
+++ public/components/list/list.js
@@ -1,5 +1,3 @@
-import { Api } from "../../modules/api.js";
-
 export class List {
     constructor(parent = document.body, submitCallback = () => {}) {
         this.parent = parent;
@@ -16,28 +14,4 @@ export class List {
         document.head.appendChild(link);
         this.parent.innerHTML = Handlebars.templates["list.hbs"]();
     }
-/*
-    onSubmit(event) {
-        event.preventDefault();
-        const inputs = this.form.querySelectorAll("input");
-        const inputsValue = {};
-        inputs.forEach((input) => {
-            inputsValue[input.id] = input.value;
-        });
-
-        Api.login(inputsValue).then(
-            (response) => {
-                if (response.status < 300) {
-                    this.SubmitCallback();
-                } else {
-                    this.showError("Неверный e-mail или пароль")
-                }
-            }
-        );
-    }
-
-    showError(message){
-        this.errorLabel.style.visibility = "visible";
-        this.errorLabel.innerHTML = message;
-    }*/
 }
