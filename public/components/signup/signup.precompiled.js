d119c80592c0f740817c855de49f88f1d7ff8303
Leonid Gukov <aarbyzevih@egmail.com>
2023-10-04 16:08:58 +0300

    new signup

--- public/components/signup/signup.precompiled.js
+++ public/components/signup/signup.precompiled.js
@@ -1,6 +1,6 @@
 (function() {
   var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
 templates['signup.hbs'] = template({"compiler":[8,">= 4.3.0"],"main":function(container,depth0,helpers,partials,data) {
-    return "<form class=\"signup\">\n    <h1>Начни свою рекламную кампанию</h1>\n    <h2>Создать новый аккаунт</h2>\n    <p>Уже есть аккаунт? <a href=\"/login\">Войти</a></p>\n\n    <input type=\"text\" placeholder=\"Имя\" id=\"f_name\">\n    <input type=\"text\" placeholder=\"Фамилия\" id=\"l_name\">\n    <input type=\"email\" placeholder=\"Почта\" id=\"login\">\n    <input type=\"password\" placeholder=\"Пароль\" id=\"password\">\n    <div class=\"error-label\"></div>\n    <button>Создать аккаунт</button>\n</form>\n";
+    return "<form class=\"signup\">\n    <p class=\"campaign-text\">Начни свою рекламную кампанию</p>\n    <h1>Создать новый аккаунт</h1>\n    <p>Уже есть аккаунт? <a href=\"/login\">Войти</a></p>\n    <div class=\"name-fields\">\n        <input type=\"text\" placeholder=\"Имя\" id=\"f_name\">\n        <input type=\"text\" placeholder=\"Фамилия\" id=\"l_name\">\n    </div>\n    <input type=\"email\" placeholder=\"Почта\" id=\"login\">\n    <input type=\"password\" placeholder=\"Пароль\" id=\"password\">\n    <div class=\"error-label\"></div>\n    <div class=\"name-fields-button\">\n        <button class=\"sign-with-account\">Войти с помощью</button>\n        <button class=\"sign-create-account\">Создать аккаунт</button>\n    </div>\n</form>\n";
 },"useData":true});
 })();
