db6d1ce9b6f0365c5092db9f19cc22f6e54094c9
almkuznetsov <almkuznetsov@mail.ru>
2023-10-22 18:19:31 +0300

    babel transpiling added

--- webpack.config.js
+++ webpack.config.js
@@ -7,5 +7,19 @@ module.exports = {
     output: {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'public')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ]
     }
 };
