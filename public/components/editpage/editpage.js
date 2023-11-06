39b8e745c7b7e65106353201885dde3f0304ad38
Leonid Gukov <aarbyzevih@egmail.com>
2023-11-06 22:28:31 +0300

    <PCF-103> bank is done

--- public/components/editpage/editpage.js
+++ public/components/editpage/editpage.js
@@ -20,6 +20,8 @@ export default class EditPage {
     this.form.addEventListener('submit', this.onSubmit.bind(this));
     this.errorLabel = this.form.getElementsByClassName('error-label')[0];
     this.errorLabel.classList.add('hidden');
+
+    this.setupEventListeners();
   }
 
   onSubmit(event) {
@@ -74,30 +76,62 @@ export default class EditPage {
     this.errorLabel.classList.add('visible')
     this.errorLabel.innerHTML = message;
   }
-}
-function toggleConfirmationBlock() {
-  var block = document.getElementById("confirmation-block");
-  var overlay = document.querySelector('.overlay');
+  toggleConfirmationBlock() {
+    var block = document.getElementById("confirmation-block");
+    var overlay = document.querySelector('.overlay');
 
-  if (block.style.display === "none") {
-      block.style.display = "block";
-      overlay.style.display = "block"; // Показать затемнение
-  } else {
-      hideConfirmationBlock(); // Вызов функции скрытия блока
+    if (block.style.display === "none") {
+        block.style.display = "block";
+        overlay.style.display = "block";    
+    } else {
+        hideConfirmationBlock(); 
+    }
+}   
+  hideConfirmationBlock() {
+    var confirmationBlock = document.getElementById("confirmation-block");
+    var replenishBanner = document.getElementById("replenish-banner");
+    var overlay = document.querySelector('.overlay');
+
+    if (confirmationBlock) {
+        confirmationBlock.style.display = "none";
+    }
+    if (replenishBanner) {
+        replenishBanner.style.display = "none";
+    }
+    overlay.style.display = "none"; // Скрыть затемнение
+  }
+  showReplenishBanner() {
+    var replenishBanner = document.getElementById("replenish-banner");
+    var overlay = document.querySelector('.overlay');
+    replenishBanner.style.display = 'block';
+    overlay.style.display = 'block'; // Показать затемнение
+}
+setupEventListeners() {
+  const confirmationCancelButton = this.parent.querySelector('.confirmation-cancel-button');
+  const replenishCancelButton = this.parent.querySelector('.replenish-cancel-button');
+  const budgetLink = this.parent.querySelector('.budget-link');
+
+  if (confirmationCancelButton) {
+    confirmationCancelButton.addEventListener('click', () => {
+      this.hideConfirmationBlock();
+    });
+  }
+
+  if (replenishCancelButton) {
+    replenishCancelButton.addEventListener('click', () => {
+      this.hideConfirmationBlock();
+    });
+  }
+
+  if (budgetLink) {
+    budgetLink.addEventListener('click', (event) => {
+      event.preventDefault();
+      this.showReplenishBanner();
+    });
   }
 }
-
-function hideConfirmationBlock() {
-  var block = document.getElementById("confirmation-block");
-  var overlay = document.querySelector('.overlay');
-  block.style.display = "none";
-  overlay.style.display = "none"; // Скрыть затемнение
 }
-
-// Добавление обработчика событий для кнопки "Отмена"
 document.addEventListener('DOMContentLoaded', function() {
-  var cancelButton = document.querySelector('.cancel-button');
-  cancelButton.addEventListener('click', function() {
-      hideConfirmationBlock(); // Скрыть блок при нажатии на кнопку "Отмена"
-  });
+  const editPage = new EditPage();
+  editPage.render();
 });
